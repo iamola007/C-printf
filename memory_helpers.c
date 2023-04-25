ce1747c50445d4618bcbd2d66ca022b2544970c1
Mafika <rudolphmafika@gmail.com>
2023-04-25 13:17:45 -0700

    removed betty warings and errors

--- memory_helpers.c
+++ memory_helpers.c
@@ -15,23 +15,21 @@ buffer_t *init_buffer(void);
  */
 unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n)
 {
-        unsigned int index;
+	unsigned int index;
 
-        for (index = 0; index < n; index++)
-        {
-                *(output->buffer) = *(src + index);
-                (output->len)++;
-
-                if (output->len == 1024)
-                {
-                        write(1, output->start, output->len);
-                        output->buffer = output->start;
-                        output->len = 0;
-                }
-
-                else
-                        (output->buffer)++;
-        }
+	for (index = 0; index < n; index++)
+	{
+		*(output->buffer) = *(src + index);
+		(output->len)++;
+		if (output->len == 1024)
+		{
+			write(1, output->start, output->len);
+			output->buffer = output->start;
+			output->len = 0;
+			}
+		else
+			(output->buffer)++;
+	}
 	return (n);
 }
 
@@ -41,8 +39,8 @@ unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n)
  */
 void free_buffer(buffer_t *output)
 {
-        free(output->start);
-        free(output);
+	free(output->start);
+	free(output);
 }
 
 /**
@@ -52,20 +50,20 @@ void free_buffer(buffer_t *output)
  */
 buffer_t *init_buffer(void)
 {
-        buffer_t *output;
+	buffer_t *output;
 
-        output = malloc(sizeof(buffer_t));
-        if (output == NULL)
-                return (NULL);
+	output = malloc(sizeof(buffer_t));
+	if (output == NULL)
+		return (NULL);
 
-        output->buffer = malloc(sizeof(char) * 1024);
-        if (output->buffer == NULL)
-        {
-                free(output);
-                return (NULL);
+	output->buffer = malloc(sizeof(char) * 1024)
+	if (output->buffer == NUL)
+	{
+		free(output);
+		return (NULL);
 	}
 	output->start = output->buffer;
-        output->len = 0;
+	output->len = 0;
 
-        return (output);
+	return (output);
 }
